343dff9309b4733ecb9f50976542f3bee63cf04c
mszkudla <259248@student.pwr.edu.pl>
2023-05-25 00:04:31 +0200

    Change structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='witch-blog-mobile-flask',
+    version='1.1',
+    packages=['app'],
+    url='witchblogflask.azurewebsites.net',
+    license='',
+    author='MichalSzkudlarek',
+    author_email='259248@student.pwr.edu.pl',
+    description='card recognition algorithm'
+)
